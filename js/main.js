7dd4037f7485e8f1ca29009916697bd36484bb0f
bourich1 <bourichanouar@gmail.com>
2023-10-10 15:15:33 +0100

    creat img file and css file and javascript file

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function  navbar(){ 
+    document.getElementById("home").innerHTML = 
+    `<div style="width:80%;;text-align:center;margin:40px; ">
+    <ul style="list-style:none;background-color:gray">
+    <li  style="margin:15px 0;"><a href="#" style="text-decoration:none;  color:white; font-size:1.5rem;">home</a></li>
+    <li style="margin:15px 0;"><a href="#" style="text-decoration:none;  color:white; font-size:1.5rem;">home</a></li>
+    <li style="margin:15px 0;"><a href="#" style="text-decoration:none;  color:white; font-size:1.5rem;">home</a></li>
+    <li style="margin:15px 0;"><a href="#" style="text-decoration:none;  color:white; font-size:1.5rem;">home</a></li>
+    <li style="margin:15px 0;"><a href="#" style="text-decoration:none;  color:white; font-size:1.5rem;">home</a></li>
+    <li style="margin:15px 0;"><a href="#" style="text-decoration:none;  color:white; font-size:1.5rem;">home</a></li>
+    </ul>
+    </div>`
+}
+function close(){
+    window.close()
+}
